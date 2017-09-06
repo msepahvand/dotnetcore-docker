f93df9f18d53536fa5a64f85356809a632a122e4
mohammad <mohammsep@gmail.com>
2017-09-07 00:51:40 +1000

    testing travis

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ dotnet test ./StudentAPI.Tests/StudentAPI.Tests.csproj
 dotnet publish StudentAPI.sln -c Release -o out
 eval $(aws ecr get-login --region ap-southeast-2)
 docker-compose build
-docker tag org/web:latest 599080142044.dkr.ecr.ap-southeast-2.amazonaws.com/org/web:latest
-docker push 599080142044.dkr.ecr.ap-southeast-2.amazonaws.com/org/web:latest
+#docker tag org/web:latest 599080142044.dkr.ecr.ap-southeast-2.amazonaws.com/org/web:latest
+#docker push 599080142044.dkr.ecr.ap-southeast-2.amazonaws.com/org/web:latest
