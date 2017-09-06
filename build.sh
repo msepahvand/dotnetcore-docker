0fae32b25257a74e4d6a10599cede25d596c483d
mohammad <mohammsep@gmail.com>
2017-09-07 00:56:03 +1000

    Trying to fix travis again

--- build.sh
+++ build.sh
@@ -4,6 +4,3 @@ dotnet restore StudentAPI.sln --no-cache
 dotnet test ./StudentAPI.Tests/StudentAPI.Tests.csproj
 dotnet publish StudentAPI.sln -c Release -o out
 eval $(aws ecr get-login --region ap-southeast-2)
-docker-compose build
-#docker tag org/web:latest 599080142044.dkr.ecr.ap-southeast-2.amazonaws.com/org/web:latest
-#docker push 599080142044.dkr.ecr.ap-southeast-2.amazonaws.com/org/web:latest
