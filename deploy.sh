744cfff45de64c3ae7c0b461258448c65f69d320
mohammad <mohammsep@gmail.com>
2017-09-07 02:24:53 +1000

    fixing travis

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -ev
 eval $(aws ecr get-login --region ap-southeast-2)
 docker-compose build
 docker tag org/web:latest 599080142044.dkr.ecr.ap-southeast-2.amazonaws.com/org/web:latest
