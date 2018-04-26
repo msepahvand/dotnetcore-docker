dbc05fc305fac425b2fb75811c5b011a50dc9ac2
mohammad <mohammsep@gmail.com>
2018-04-27 01:05:26 +1000

    adding --no-include-email flag to ecr get-login due to Invalid '-e' flag error

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ev
-eval $(aws ecr get-login --region ap-southeast-2)
+eval $(aws ecr get-login --region ap-southeast-2 --no-include-email)
 docker-compose build
 docker tag studentapi/web:latest 599080142044.dkr.ecr.ap-southeast-2.amazonaws.com/studentapi/web:latest
 docker push 599080142044.dkr.ecr.ap-southeast-2.amazonaws.com/studentapi/web:latest
